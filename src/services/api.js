f11c6becfdbd9fbc64b03e10dde94aee2c6864b6
ClemersonDBA <clemerson20@hotmail.com>
2020-06-30 17:56:59 -0300

    env config

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import 'dotenv';
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_URL, 
+    baseURL: process.env.REACT_APP_API_URL, 
 });
 
 export default api;
